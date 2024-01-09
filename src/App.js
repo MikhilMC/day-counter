fcf24bb7fca052eb0b2bf34ed3f29f5c14befbba
MikhilMC <mikhilmohan13@gmail.com>
2024-01-09 10:51:12 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date(Date.now());
+  date.setDate(date.getDate() + count);
+
+  function handleStepDecrease() {
+    setStep((s) => s - 1);
+  }
+
+  function handleStepIncrease() {
+    setStep((s) => s + 1);
+  }
+
+  function handleCountDecrease() {
+    setCount((c) => (step === 0 ? c : step > 0 ? c - step : c + step));
+  }
+
+  function handleCountIncrease() {
+    setCount((c) => (step === 0 ? c : step > 0 ? c + step : c - step));
+  }
+
+  return (
+    <div>
+      <div className="button-group">
+        <div className="button-row">
+          <button onClick={handleStepDecrease}>-</button>
+          <span> Step: {step} </span>
+          <button onClick={handleStepIncrease}>+</button>
+        </div>
+        <div className="button-row">
+          <button onClick={handleCountDecrease}>-</button>
+          <span> Count: {count} </span>
+          <button onClick={handleCountIncrease}>+</button>
+        </div>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
     </div>
   );
 }
